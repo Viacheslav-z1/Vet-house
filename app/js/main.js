2c2e5c91059f0ce42c91c627d377e5d865f6e67a
Slavik <slavik.zaliotnyi@gmail.com>
2023-09-16 16:45:31 +0300

    added form section

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,43 @@
 
 
 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    slidesPerView: 4,
-    grid: {
-      rows: 2,
-    },
-    navigation: {
-        nextEl: null, // Отключение кнопки "Следующий слайд"
-        prevEl: null, // Отключение кнопки "Предыдущий слайд"
-      },
-    spaceBetween: 30,
-  });
+const swiper = new Swiper('#swiper1', {
+  // Optional parameters
+  direction: 'horizontal',
+  slidesPerView: 4,
+  grid: {
+    rows: 2,
+  },
+  navigation: {
+    nextEl: null, // Отключение кнопки "Следующий слайд"
+    prevEl: null, // Отключение кнопки "Предыдущий слайд"
+  },
+  spaceBetween: 30,
+});
 
+
+
+const swiper2 = new Swiper('#swiper2', {
+  // Optional parameters
+  direction: 'horizontal',
+  slidesPerView: 4,
+  spaceBetween: 30,
+  nextEl: '.swiper-button-next', // Селектор для кнопки "Следующий слайд"
+  prevEl: '.swiper-button-prev', // Селектор для кнопки "Предыдущий слайд"
+
+
+
+});
+
+const swiperPrev = document.getElementById('swiperPrev')
+const swiperNext = document.getElementById('swiperNext')
+
+swiperPrev.addEventListener('click', () => {
+  swiper2.slidePrev();
+})
+swiperNext.addEventListener('click', () => {
+  swiper2.slideNext();
+})
 
 
 
