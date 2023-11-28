ac7c1e1720017f96652b086278a2e7691d7fee19
Slavik <slavik.zaliotnyi@gmail.com>
2023-11-28 21:42:55 +0200

    fix validation number and added form data

--- app/js/formValidation.js
+++ app/js/formValidation.js
@@ -1,53 +1,66 @@
 
-function validator(form, inputs){
+function validator(form, inputs) {
 
-inputs.forEach( input =>{
-    input.addEventListener('input', ()=>{
-        if(input.value.trim() == '' || input.value == 'default'){
+    inputs.forEach(input => {
+        input.addEventListener('input', () => checkAllValidInputs(input))
+
+    })
+
+    inputs.forEach(input => {
+        input.addEventListener('change', () => checkAllValidInputs(input))
+
+    })
+
+    function checkAllValidInputs(input) {
+        if (input.value.trim() == '' || input.value == 'default' || input.value.includes("_")) {
             input.classList.add('error');
-           
-        } else{
+        } else {
             input.classList.remove('error');
-         
+
+        }
+    }
+
+
+    function validation() {
+        let dataValidated = 0;
+
+        inputs.forEach(input => {
+            if (input.value.trim() == '' || input.value == 'default' || input.value.includes("_")) {
+                input.classList.add('error');
+            } else {
+                input.classList.remove('error')
+            }
+        })
+
+        inputs.forEach(input => {
+            if (input.classList.contains('error')) {
+                dataValidated += 1;
+            }
+        })
+
+
+        if (dataValidated == 0) {
+            return 'true'
+        } else {
+            return 'false'
+        }
+    }
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        validation();
+
+        if (validation() == 'true') {
+            console.log('data send');
+            let formData = new FormData(form);
+            let data = {};
+            for (const [key, value] of formData) {
+                data[key] = value;
+            }
+            JSON.stringify(data);
+            console.log(data);
         }
     })
-   
-})
-
-
-function validation(){
-    let dataValidated = 0;
-  
-    inputs.forEach( input =>{
-        if(input.value.trim() == '' || input.value == 'default'){
-            input.classList.add('error');
-        } else{
-            input.classList.remove('error')
-        }
-    })
-
-    inputs.forEach( input =>{
-        if(input.classList.contains('error')){
-            dataValidated += 1;
-        }  
-    })
-
-
-    if( dataValidated == 0){
-        return 'true'
-    }else{
-        return 'false'
-    }
-}
-
-form.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    validation();
-
-    if( validation() == 'true'){
-        console.log('data send');
-    }
-})
 
 }
 
